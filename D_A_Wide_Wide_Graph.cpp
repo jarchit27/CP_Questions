30280213508053aef9d69202147eca977da690b9
jarchit27 <j.archit134@gmail.com>
2025-05-08 18:20:39 +0530

    first commit

--- D_A_Wide_Wide_Graph.cpp
+++ D_A_Wide_Wide_Graph.cpp
@@ -8,62 +8,6 @@ typedef long double doo;
 typedef tree<ll,null_type,less<ll>,rb_tree_tag,tree_order_statistics_node_update> ordered_set;
 typedef tree<ll, ll, std::less<ll>, rb_tree_tag, tree_order_statistics_node_update> ordered_map;
 typedef tree<ll, null_type, less_equal <ll>, rb_tree_tag, tree_order_statistics_node_update> ordered_multiset;
-// find_by_order : returns an iterator to the k-th smallest element (0-based index) 
-//order_of_key(value): Returns the count of elements strictly smaller than value.
-#define yes cout<<"YES"<<endl;
-#define no cout<<"NO"<<endl;
-#define ll_max INT64_MAX
-#define ll_min INT64_MIN
-ll N = 1e5;
-ll inf = 1e17;
-ll MOD = 1e9+7;
-ll power(ll a, ll b){
-    ll result = 1;
-    while (b)
-    {
-        if (b & 1)
-            result = result * a;
-        a = a * a;
-        b >>= 1;
-    }
-    return result;
-}
-ll power_mod(ll a, ll b, ll modulo){
-    ll result = 1;
-    while (b)
-    {
-        if (b & 1)
-            result = (result * a) % modulo;
-        a = (a * a) % modulo;
-        b >>= 1;
-    }
-    return result;
-}
-string num_tobin(ll n){
-    string s = bitset<32>(n).to_string();
-    return s;
-}
-vector<bool> is_prime(1e5+1, true);
-vector<ll> prime_number;
-void sieve()
-{
-    ll N = 1e5;
-    is_prime[0] = false;
-    is_prime[1] = false;
-    for(ll i = 2; i * i <= N; i++)
-    {
-        if (is_prime[i])
-            for(ll j = i * i; j <= N; j += i)
-                is_prime[j] = false;
-    }
-    for (int p = 2; p <= N; p++)
-        if (is_prime[p])
-            prime_number.push_back(p);
-}
-ll mod_inverse(ll a, ll m)
-{
-    return power_mod(a, m - 2, m);
-}
 
 void bfs(vector<vector<ll>> &adj, ll n , ll start, vector<ll> &dist )
 {
